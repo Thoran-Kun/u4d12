f6c9c8960d789b1a2e02eb01feec8985f4869439
Salvatore <pepesalvatore12@gmail.com>
2026-01-20 17:26:55 +0100

    finish

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Persistence;
 import org.example.dao.EventoDAO;
 import org.example.entites.Evento;
 import org.example.entites.TipoEvento;
+import org.example.exceptions.NotFoundException;
 
 public class Application {
 private static final EntityManagerFactory entityManagerFactory =
@@ -13,8 +14,36 @@ private static final EntityManagerFactory entityManagerFactory =
     public static void main(String[] args) {
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
+        //Questo oggetto gestisce le interazioni col DB dei nuovi eventi che vogliamo creare o eliminare
         EventoDAO ev = new EventoDAO(entityManager);
-        Evento party = new Evento("party", "2001-01-01", "party privato", TipoEvento.PRIVATO);
 
+        //Qui creiamo appunto i nuovi eventi
+        Evento party = new Evento("party", "2001-01-01", "party privato", TipoEvento.PRIVATO);
+        Evento festaCapodanno = new Evento("Capodanno", "2022-12 -31", "festa di Capodanno", TipoEvento.PUBBLICO);
+        Evento 
+        //facciamo partire il commit per salvare i nostri eventi
+        //----------------------------------- SAVE ------------------------------
+        ev.save(party);
+        ev.save(festaCapodanno);
+
+        //--------------------------------- FIND BY ID ------------------------------
+        try {
+            Evento eventoFromDB = ev.findById(1);
+            System.out.println(eventoFromDB);
+        } catch (NotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        //------------------------------- FIND BY ID AND DELETE -------------------------
+        try{
+            ev.findByIdAndDelete(1);
+        } catch (NotFoundException ex){
+            System.out.println(ex.getMessage());
+        }
+
+
+
+        entityManager.close();
+        entityManagerFactory.close();
     }
 }
--- src/main/java/org/example/dao/EventoDAO.java
+++ src/main/java/org/example/dao/EventoDAO.java
@@ -3,6 +3,7 @@ package org.example.dao;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import org.example.entites.Evento;
+import org.example.exceptions.NotFoundException;
 
 public class EventoDAO {
     private final EntityManager entityManager;
@@ -12,7 +13,46 @@ public class EventoDAO {
     }
 
     public void save(Evento newEvento){
-        //
+        //1.chiedo di creare una nuova transazione
         EntityTransaction transaction = entityManager.getTransaction();
+
+        //2. Parte la transazione
+        transaction.begin();
+
+        //3. Aggiungo newEvento
+        entityManager.persist(newEvento);
+
+        //4.COMMITTOOO TUTTO
+        transaction.commit();
+
+        //5.log di salvataggio
+        System.out.printf("EVENTO " + newEvento.getTitolo() + " salvato nel DB");
+
+    }
+
+    public Evento findById(long eventoId) {
+        Evento found = entityManager.find(Evento.class, eventoId);
+        if(found == null) throw new NotFoundException(eventoId);
+        return found;
+    }
+
+    public void findByIdAndDelete(long eventoId){
+        //1. Cerco il mio evento da cancellare
+        Evento found = this.findById(eventoId);
+
+        //2. Chiedo di creare una nuova transazione
+        EntityTransaction transaction = entityManager.getTransaction();
+
+        //3. Faccio partire quindi la transazione
+        transaction.begin();
+
+        //4. RIMUOVO dal Persistence l'oggetto
+        entityManager.remove(found);
+
+        //5. Per cancellare dal database definitivamente l'evento devo committare la transazione
+        transaction.commit();
+
+        //6. Log di avvenuta correttamente cancellazione
+        System.out.println("L'evento " + eventoId + " è stato eliminato correttamente");
     }
 }
