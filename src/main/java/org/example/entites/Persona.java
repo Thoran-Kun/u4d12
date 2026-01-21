567b28979722c30fbf7591206c75fb79fe9c9462
Salvatore <pepesalvatore12@gmail.com>
2026-01-21 16:33:42 +0100

    add class

--- src/main/java/org/example/entites/Persona.java
+++ src/main/java/org/example/entites/Persona.java
@@ -1,4 +1,29 @@
 package org.example.entites;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Persona {
+    @OneToMany(mappedBy = "evento")
+    List<Partecipazione> partecipazioni = new ArrayList<>();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    @Column(name = "nome", nullable = false)
+    private String nome;
+    @Column (name = "cognome", nullable = false)
+    private String cognome;
+    @Column (name = "email", nullable = false)
+    private String email;
+    @Column (name = "data_di_nascita", nullable = false)
+    private LocalDate dataDiNascita;
+    @Column (name = "sesso", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Sesso sesso;
+
+
 }
---
+++ src/main/java/org/example/entites/Sesso.java
@@ -0,0 +1,5 @@
+package org.example.entites;
+
+public enum Sesso {
+    MASCHIO, FEMMINA
+}
